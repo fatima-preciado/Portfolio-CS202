5a0ae409919e2810f60d28aa7ef658bb7473ddf5
conejiito <111170527+conejiito@users.noreply.github.com>
2024-05-30 14:22:07 -0700

    final-project

---
+++ final-project/src/pages/projects/project1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './projects.css'
+
+function project1() {
+  return (
+    <div>
+        <h1> hello </h1>
+    </div>
+  );
+}
+
+export default project1;
---
+++ final-project/src/pages/projects/project2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './projects.css'
+
+function project2() {
+  return (
+    <div>
+        <h1> goodbye </h1>
+    </div>
+  );
+}
+
+export default project2;
